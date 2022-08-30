ccdcd81580d328dc6e41c24e56b883ff221920aa
JAWA Production <94666849+Raven1K@users.noreply.github.com>
2022-08-30 23:19:01 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Fontawesome Icons-->
+    <link rel="stylesheet" href="fonts/css/all.css">
+
+    <title>Welcome to Red Planet</title>
+  </head>
+
+  <body>
+    <?php require_once 'navbar.php';?>
+	<div>
+	<img src="images/redplanet.jpg" class="img-fluid" alt="Red Planet">
+	</div>
+    <div class="container">
+	<h1 class="page-header text-center">List of Martian with their corresponding Bases</h1>
+	<div class="row">
+		<div class="col-sm-12 col-sm-offset-2">
+			<a href="#add" class="btn btn-primary" data-toggle="modal"><i class="fa-solid fa-user-plus"></i></span> Add Member</a>
+      <font-awesome-icon icon="fa-solid fa-circle-plus" />
+            <div id="alert" class="alert alert-info text-center" style="margin-top:20px; display:none;">
+            	<button class="close"><span aria-hidden="true">&times;</span></button>
+                <span id="alert_message"></span>
+            </div>  
+			<table class="table table-bordered table-striped" style="margin-top:20px;">
+				<thead>
+					<th>ID</th>
+					<th>Firstname</th>
+					<th>Lastname</th>
+					<th>Base Name</th>
+					<th>Action</th>
+				</thead>
+				<tbody id="tbody"></tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+	<!-- Modals -->
+	<?php include('modal.html'); ?>
+	<script src="js/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+	<script src="js/app.js"></script>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+
+</body>
+</html>
