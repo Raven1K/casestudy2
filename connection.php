4a4eb89ed0bda493770a6ef71300ddae00cd86a4
JAWA Production <94666849+Raven1K@users.noreply.github.com>
2022-08-31 17:06:28 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
 Class Connection{
  
-	private $server = "mysql:host=us-cdbr-east-06.cleardb.net; dbname=db_mars";
+	private $server = "mysql:host=us-cdbr-east-06.cleardb.net; dbname=heroku_fe19d1fa6e9b61d";
 	private $username = "bbc7383f40e78b";
 	private $password = "4fbff9bd";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
