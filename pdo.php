5d94f52b5a4c720335debd9efdb446b20f1af977
s0greedy <70991219+jay324lerap@users.noreply.github.com>
2022-09-01 10:37:57 +0800

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -7,12 +7,11 @@
 // See the "errors"
 //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-$servername = "localhost";
-$dbname = "dbmartian";
-$username = "root";
+$servername = "mysql:host=us-cdbr-east-06.cleardb.net; dbname=heroku_fe19d1fa6e9b61d";
+$dbname = "bbc7383f40e78b";
+$username = "4fbff9bd";
 $password = "";
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=dbmartian','root', '');
+$pdo = new PDO('mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_fe19d1fa6e9b61d','bbc7383f40e78b', '4fbff9bd');
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
 
