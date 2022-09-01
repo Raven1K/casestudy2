0431a26ab2715048b62e3fe9089456c911cd0643
s0greedy <70991219+jay324lerap@users.noreply.github.com>
2022-09-01 22:25:36 +0800

    Update supplies.php

--- supplies.php
+++ supplies.php
@@ -40,7 +40,7 @@
                                 <?php
 
                                     $query = "SELECT base.base_name, supply.name, supply.description, inventory.quantity
-				    		FROM (SELECT * FROM inventory LEFT JOIN base WHERE inventory.base_id = base.base_id) 
+				    		FROM (SELECT * FROM inventory LEFT JOIN base ON inventory.base_id = base.base_id) 
 						RIGHT JOIN supply ON supply.supply_id = inventory.supply_id
 						ORDER BY supply.supply_id";
                                     $statement = $conn->prepare($query);
